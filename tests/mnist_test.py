bd42d584caecb87c1aefb86d868d23acf3c02290
nabehide <iasoyv@gmail.com>
2017-11-03 03:09:11 +0900

    add test for mnist

--- tests/mnist_test.py
+++ tests/mnist_test.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import pytest
 
 import sys
 import os
@@ -6,17 +6,30 @@ sys.path.append(os.getcwd() + '/MNIST')
 from VisualizeMNIST import Scribble
 
 
-class TestScribble(TestCase):
+# class TestScribble(TestCase):
+class TestClass:
 
-    def test_on_pressed(self):
-        self.assertTrue(True)
+    @pytest.fixture()
+    def scribble(request):
+        return Scribble(modelName=os.getcwd() + "/MNIST/20160818_MNIST.model")
+
+    def test_on_pressed(self, scribble):
+        return True
 
     def test_on_dragged(self):
-        self.assertTrue(True)
+        return True
 
-    def test_judge(self):
-        scribble = Scribble(
-            modelName=os.getcwd() + "/MNIST/20160818_MNIST.model"
-        )
+    def test_judge(self, scribble):
         scribble.judge()
         assert 5 == scribble.val.index(max(scribble.val))
+
+    def test_clear(self, scribble):
+        scribble.clear()
+        return True
+
+    def test_create_window(self, scribble):
+        scribble.create_window()
+        return True
+
+    def test_run(self, scribble):
+        return True
